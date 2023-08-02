5b349cfe862a1baf372975b751365ee1d8f270b4
Krister <randyersson@gmail.com>
2023-08-02 03:42:28 +0200

    Add real air resistance.

--- src/Pendulum.java
+++ src/Pendulum.java
@@ -1,17 +1,17 @@
 public final class Pendulum {
     private static final double GRAVITY = 9.82;
-    private static final double DAMPING_FACTOR = 0.99;
-
     private final int radius;
     private final double length;
     private final double omega;
-    private double currentAngle;
-
+    private final double currentAngle;
+    private static final double K_STRING = 9.18 * Math.pow(10, -4);
+    private static final double K_BOB = 7.24 * Math.pow(10, -4);
+    private static final double K_COEFFICIENT = K_STRING + K_BOB;
 
     public Pendulum(int radius, double length, double initialTheta) {
         this.radius = radius;
         this.length = length;
-        this.omega = (Math.sqrt(GRAVITY/length));
+        this.omega = (Math.sqrt(GRAVITY/length) - ((K_COEFFICIENT * K_COEFFICIENT)/4));
         this.currentAngle = initialTheta;
     }
 
@@ -24,16 +24,12 @@ public final class Pendulum {
     }
 
     public double getTheta(double t) {
-        return this.currentAngle * Math.cos(this.omega * t);
+        return this.currentAngle * Math.exp((-K_COEFFICIENT * t)/2) *
+                (Math.cos(this.omega * t) + (K_COEFFICIENT/(2 * this.omega)) * Math.sin(this.omega * t));
     }
 
     public double getRadius() {
         return this.radius;
     }
-
-    public void applyDamping() {
-        this.currentAngle *= DAMPING_FACTOR;
-    }
-
 
 }
--- src/PendulumPanel.java
+++ src/PendulumPanel.java
@@ -6,17 +6,16 @@ public class PendulumPanel extends JPanel {
     private static final int WIDTH = 600;
     private final Pendulum pendulum;
     private double time = 0;
-    private static final double DELTA_TIME = 1;
+    private static final double DELTA_TIME = 0.75;
     private static final int REFRESH_RATE_60HZ = 17;
 
     public PendulumPanel(Pendulum pendulum) {
         this.pendulum = pendulum;
-        new Timer(REFRESH_RATE_60HZ, e -> update(pendulum)).start();
+        new Timer(REFRESH_RATE_60HZ, e -> update()).start();
     }
 
-    private void update(Pendulum pendulum) {
+    private void update() {
         time += DELTA_TIME;
-        pendulum.applyDamping();
         repaint();
     }
 
@@ -25,8 +24,8 @@ public class PendulumPanel extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g2d);
         int endPointX = (int) (HEIGHT/2 + pendulum.getXCord(time));
-        int endPointY = (int) (HEIGHT/2 - pendulum.getYCord(time));
-        g2d.drawLine(HEIGHT/2, WIDTH/2, endPointX, endPointY);
+        int endPointY = (int) (HEIGHT/5 - pendulum.getYCord(time));
+        g2d.drawLine(HEIGHT/2, WIDTH/5, endPointX, endPointY);
         g2d.fillOval((int) (endPointX - pendulum.getRadius()/2),
                 (int) (endPointY - pendulum.getRadius()/2),
                 (int) pendulum.getRadius(),
